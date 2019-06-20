6fc3f17a6bd6ee87fa2d1f6a6a0e7c3095d72f97
vietbq <vietbq@nal.vn>
2019-06-20 23:52:22 +0700

    Update UI

--- resources/assets/js/components/ChatMessages.vue
+++ resources/assets/js/components/ChatMessages.vue
@@ -1,16 +1,9 @@
 <template>
     <ul class="chat">
         <li class="left clearfix" v-for="message in messages">
-            <div>
-                <div class="header">
-                    <strong class="primary-font">
-                        {{ message.user }}
-                    </strong>
-                </div>
                 <p v-bind:class="[message.is_me ? 'me' : 'bot']">
-                    {{ message.message }}
+                    {{ message.user }} : {{ message.message }}
                 </p>
-            </div>
         </li>
     </ul>
 </template>

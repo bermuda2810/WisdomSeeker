1c08538936cdcae0dd9f942c369c102842fa3e84
vietbq <vietbq@nal.vn>
2019-06-23 23:05:15 +0700

    Update UI

--- resources/assets/js/components/ChatForm.vue
+++ resources/assets/js/components/ChatForm.vue
@@ -1,18 +1,14 @@
 <template>
-    <form class="input-group">
+    <form>
         <input id="m"
                type="text"
                name="message"
-               class="form-control
-               input-sm"
                v-model="newMessage"
                @keydown.enter.prevent="sendMessage"
                :disabled="submitted">
-        <span class="input-group-btn">
-            <button class="btn btn-primary btn-sm" id="btn-chat" @click.prevent="sendMessage" :disabled="submitted">
+            <button id="btn-chat" @click.prevent="sendMessage" :disabled="submitted">
                 Send
             </button>
-        </span>
     </form>
 
 </template>
